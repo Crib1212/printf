b576c3b374aa72ff176e155ba0aec9f84b93b41a
chukwudiezeatu <chukwudiezeatu@gmail.com>
2023-08-25 13:27:20 -0700

    updated

--- main.h
+++ main.h
@@ -1,13 +1,3 @@
-#ifndef PRINT_F
-#define PRINT_F
-
-/* Files  in header.. */
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <limits.h>
-=======
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -26,47 +16,26 @@
  ** Return: int
  **/
 
-/* Prototype of the function*/
-int print_unint(va_list);
-int print_hex_l(va_list);
-int print_hex_u(va_list);
-int _printf(const char *format, ...);
-int put_char(char);
-size_t cal_val(size_t n, int base);
-int print_bin(va_list);
-int format_spec(const char *, int *, int *, va_list);
-int print_ch(va_list);
-int print_int(va_list);
-int print_oct(va_list);
-int print_str(va_list);
+typedef struct flags_format
+{
+        int plus;
+        int hash;
+        int space;
+} flags_t;
 
-/**
- * struct conv - Template for the format conversion
- * @c: the format of specifiers
- * @f: pointr to the corresponding func @c
- */
-typedef struct conv spec_conv;
-struct conv
+
+typedef struct format_handler
 {
 	char c;
-
-	int (*f)(va_list);
-};
-
-#define OCT 8
-#define HEX 16
-#define BIN 2
-
-=======
-int (*f)(va_list ap, flags_t *f);
+	int (*f)(va_list ap, flags_t *f);
 } fo_handler;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _puts(char *leo);
+int _puts(char *str);
 int (*set_print(char s))(va_list, flags_t *);
 int call_flag(char s, flags_t *f);
-int print_leo(va_list l, flags_t *f);
+int print_str(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 int print_digits(va_list l, flags_t *f);
 void print_number(int n);
@@ -82,12 +51,4 @@ int print_rev(va_list l, flags_t *f);
 int print_non_printable(va_list l, flags_t *f);
 int print_addr(va_list l, flags_t *f);
 int print_percent(va_list l, flags_t *f);
-
-typedef struct flags_format
-{
-	int plus;
-	int hash;
-	int space;
-} flags_t;
-
 #endif
