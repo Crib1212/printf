5b84028380599b52dcd81885d559263afb2b5456
chukwudiezeatu <chukwudiezeatu@gmail.com>
2023-08-25 11:52:12 -0700

    updated

--- leo/main.h
+++ leo/main.h
@@ -1,5 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 
-<<<<<<< HEAD
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
  ** struct format_handler - structure containing char and int
  ** @c: character for format specifier
@@ -12,41 +16,39 @@
  ** Return: int
  **/
 
+typedef struct flags_format
+{
+        int plus;
+        int hash;
+        int space;
+} flags_t;
+
 
 typedef struct format_handler
 {
 	char c;
-	int (*f)(va_list ap, flags_f *f);
+	int (*f)(va_list ap, flags_t *f);
 } fo_handler;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *leo);
-int (*set_print(char s))(va_list, flags_f *);
-int call_flag(char s, flags_f *f);
-int print_leo(va_list l, flags_f *f);
-int print_char(va_list l, flags_f *f);
-int print_digits(va_list l, flags_f *f);
+int (*set_print(char s))(va_list, flags_t *);
+int call_flag(char s, flags_t *f);
+int print_leo(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+int print_digits(va_list l, flags_t *f);
 void print_number(int n);
-int print_unsigned(va_list l, flags_f *f);
+int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
-int print_hex(va_list l, flags_f *f);
-int print_HEX(va_list l, flags_f *f);
-int print_bin(va_list l, flags_f *f);
-int print_oct(va_list l, flags_f *f);
+int print_hex(va_list l, flags_t *f);
+int print_HEX(va_list l, flags_t *f);
+int print_bin(va_list l, flags_t *f);
+int print_oct(va_list l, flags_t *f);
 char *convert(unsigned long int num, int base, int lowercase);
-int encode_rot13(va_list l, flags_f *f);
-int print_rev(va_list l, flags_f *f);
-int print_non_printable(va_list l, flags_f *f);
-int print_addr(va_list l, flags_f *f);
-int print_percent(va_list l, flags_f *f);
-
-typedef struct flags_format
-{
-	int plus;
-	int hash;
-	int space;
-} flags_f;
+int encode_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_non_printable(va_list l, flags_t *f);
+int print_addr(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *f);
 #endif
-=======
->>>>>>> e79fdc5ecf647b39a05262a85532f0dcb080a5d6
